5da1cef9ce51f40d93197680bfc1fd5ba1df16f3
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-07-09 16:19:06 +0100

    Adding additional principal permissions

--- scripts/service-connections.ps1
+++ scripts/service-connections.ps1
@@ -53,6 +53,10 @@ $subscriptions = @(
                 Role  = "Contributor"
                 Scope = "/subscriptions/db34f572-8b71-40d6-8f99-f29a27612144/resourceGroups/rg-platform-frontdoor-prd-uksouth"
             }
+            @{
+                Role  = "Contributor"
+                Scope = "/subscriptions/903b6685-c12a-4703-ac54-7ec1ff15ca43/resourceGroups/rg-platform-webapps-prd-uksouth"
+            }
         )
         BuiltInRoles     = @(
             "158c047a-c907-4556-b7ef-446551a6b5f7" # Cloud application administrator
