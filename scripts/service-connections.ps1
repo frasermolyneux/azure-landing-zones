913aea1c2a803aaff0786b63197aef268d0434dc
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-07-12 20:06:42 +0100

    Adding permissions and new connection

--- scripts/service-connections.ps1
+++ scripts/service-connections.ps1
@@ -93,10 +93,42 @@ $subscriptions = @(
                 Role  = "Contributor"
                 Scope = "/subscriptions/32444f38-32f4-409f-889c-8e8aa2b5b4d1"
             }
+            @{
+                Role  = "Contributor"
+                Scope = "/subscriptions/db34f572-8b71-40d6-8f99-f29a27612144/resourceGroups/rg-platform-dns-prd-uksouth"
+            }
+            @{
+                Role  = "Contributor"
+                Scope = "/subscriptions/db34f572-8b71-40d6-8f99-f29a27612144/resourceGroups/rg-platform-frontdoor-prd-uksouth"
+            }
+            @{
+                Role  = "Contributor"
+                Scope = "/subscriptions/903b6685-c12a-4703-ac54-7ec1ff15ca43/resourceGroups/rg-platform-webapps-prd-uksouth"
+            }
+            @{
+                Role  = "Contributor"
+                Scope = "/subscriptions/903b6685-c12a-4703-ac54-7ec1ff15ca43/resourceGroups/rg-platform-apim-prd-uksouth"
+            }
         )
         BuiltInRoles     = @(
             "158c047a-c907-4556-b7ef-446551a6b5f7" # Cloud application administrator
             "9360feb5-f418-4baa-8175-e2a00bac4301" # Directory Writers
+        )
+    }
+    @{
+        ConnectionName   = "spn-ado-XtremeIdiots-xtremeidiots-portal-webapps"
+        SubscriptionName = "sub-platform-strategic"
+        SubscriptionId   = "903b6685-c12a-4703-ac54-7ec1ff15ca43"
+        AzDoProject      = "XtremeIdiots"
+        Workload         = "xtremeidiots-portal-webapps"
+        Permissions      = @(
+            @{
+                Role  = "Contributor"
+                Scope = "/subscriptions/903b6685-c12a-4703-ac54-7ec1ff15ca43/resourceGroups/rg-platform-webapps-prd-uksouth"
+            }
+        )
+        BuiltInRoles     = @(
+            "158c047a-c907-4556-b7ef-446551a6b5f7" # Cloud application administrator
         )
     }
     @{
@@ -118,7 +150,18 @@ $subscriptions = @(
 $subscriptions | ForEach-Object {
     $spn = (az ad sp list --filter "displayName eq '$($_.ConnectionName)'") | ConvertFrom-Json
     if ($spn.Count -eq 0) {
-        az ad sp create-for-rbac --display-name $_.ConnectionName | ConvertFrom-Json
+        $spn = az ad sp create-for-rbac --display-name $_.ConnectionName | ConvertFrom-Json
+        $env:AZURE_DEVOPS_EXT_AZURE_RM_SERVICE_PRINCIPAL_KEY = $spn.password
+
+        az devops service-endpoint azurerm create `
+            --azure-rm-service-principal-id $spn.appId `
+            --azure-rm-subscription-id $_.SubscriptionId `
+            --azure-rm-subscription-name $_.SubscriptionName `
+            --azure-rm-tenant-id $tenantId `
+            --name $_.ConnectionName `
+            --organization $organization `
+            --project $_.AzDoProject
+
         $spn = (az ad sp list --filter "displayName eq '$($_.ConnectionName)'") | ConvertFrom-Json
     }
     
@@ -133,21 +176,5 @@ $subscriptions | ForEach-Object {
         . "./azure-landing-zones/scripts/functions/GrantRoleToPrincipal.ps1" `
             -principalId $spn.id `
             -roleDefinitionId $_
-    }
-
-    $env:AZURE_DEVOPS_EXT_AZURE_RM_SERVICE_PRINCIPAL_KEY = $spn.password
-
-    $serviceEndpoints = az devops service-endpoint list --organization $organization --project $_.AzDoProject | ConvertFrom-Json
-    $existingServiceEndpoint = $serviceEndpoints | Where-Object { $_.authorization.parameters.serviceprincipalid -eq $spn.appId }
-
-    if ($null -eq $existingServiceEndpoint) {
-        az devops service-endpoint azurerm create `
-            --azure-rm-service-principal-id $spn.appId `
-            --azure-rm-subscription-id $_.SubscriptionId `
-            --azure-rm-subscription-name $_.SubscriptionName `
-            --azure-rm-tenant-id $tenantId `
-            --name $_.ConnectionName `
-            --organization $organization `
-            --project $_.AzDoProject
     }
 }
